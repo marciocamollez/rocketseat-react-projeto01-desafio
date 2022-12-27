93484125db6073a6cc6b50555fd1e693c7b67655
Marcio Camollez <marciocamollez@hotmail.com>
2022-12-27 14:20:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola</h1>
+    </div>
+  );
+}
+
+export default App;
